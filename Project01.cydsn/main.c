52d0847380af60c610fac2eb46a4deade5ab380b
Chloé <chloejulie.charlotte@mail.polimi.it>
2022-05-04 17:20:08 +0200

    Added header and source files

--- Project01.cydsn/main.c
+++ Project01.cydsn/main.c
@@ -9,18 +9,38 @@
  *
  * ========================================
 */
+#include <stdio.h>
 #include "project.h"
+#include "I2C_Interface.h"
 
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
-
+    
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    
+    I2C_Peripheral_Start();
+    UART_Start();
+    
+    CyDelay(5); //"The boot procedure is complete about 5 ms after device power-up."
+    
+    // String to print out messages on the UART
+    char message[50] = {'\0'};
+    
+    // Check which devices are present on the I2C bus
+    for(int i = 0 ; i < 128; i++ )
+    {
+        if (I2C_Peripheral_IsDeviceConnected(i))
+        {
+            // print out the address is hex format
+            sprintf(message, "Device 0x%02X is connected\r\n", i);
+            UART_PutString(message);
+        } 
+    }
 
     for(;;)
     {
         /* Place your application code here. */
-        //Maximum is the best name
     }
 }
 
