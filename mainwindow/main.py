b6c594a81b0086185251d9c22dc66a5907cdf009
Patrick Franke <patrickfranke@outlook.de>
2022-05-13 17:45:22 +0200

    Able to read data over bluetooth and modified GUI

--- mainwindow/main.py
+++ mainwindow/main.py
@@ -1,23 +1,8 @@
-## ---------------------------------------------------------------------------------------
-## 
-## PYQT APPLICATION FOR DATA VISUALIZATION OF ACCELEROMETER DATA + SLEEP POSITION ANALYSIS
-## 
-## ---------------------------------------------------------------------------------------
 
 
-## Importing necessary libraries
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QPushButton, QVBoxLayout
 
 
-## Starting the PyQt application
-app = QApplication([])
 
-
-## Creating the widgets
-window = QWidget()
-label = QLabel('Output of accelerometer data:')
-button1 = QPushButton('Pause')
-button2 = QPushButton('Close')
 
 
 ## Setting the layout of the widget
@@ -25,9 +10,3 @@ layout = QVBoxLayout()
 layout.addWidget(label)
 layout.addWidget(button1)
 layout.addWidget(button2)
-window.setLayout(layout)
-
-
-## Running the application
-window.show()
-app.exec()
