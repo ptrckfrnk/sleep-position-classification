a50f4a62c13a04c1c0d39561b650d28bb13d5e77
Chloé <chloejulie.charlotte@mail.polimi.it>
2022-07-06 16:38:13 +0200

    Added correct PSoC project

---
+++ Sleep_position_classifier_PSoC.cydsn/ErrorCodes.h
@@ -0,0 +1,28 @@
+/* ===============================================
+ *
+ * Alfonso Massimo, Canavate Chloé, Franke Patrick
+ * Projet 05 - Sleep Position Classifier
+ * Electronic Technologies and Biosensor Laboratory
+ * Academic year 2021 - 2022
+ * ErrorCodes.h file
+ * \brief Error codes definition
+ *
+ * =============================================== */
+
+
+/**
+*   \brief Error codes.
+* 
+*   This definition defines several error codes that will
+*   be used throughout the project.
+*/
+#ifndef __ERRORCODES_H
+    #define __ERRORCODES_H
+    
+    typedef enum {
+        NO_ERROR,           ///< No error generated
+        ERROR               ///< Error generated
+    } ErrorCode;
+
+#endif
+/* [] END OF FILE */
---
+++ Sleep_position_classifier_PSoC.cydsn/cyapicallbacks.h
@@ -0,0 +1,21 @@
+/* ===============================================
+ *
+ * Alfonso Massimo, Canavate Chloé, Franke Patrick
+ * Projet 05 - Sleep Position Classifier
+ * Electronic Technologies and Biosensor Laboratory
+ * Academic year 2021 - 2022
+ * cyapicallbacks.h file
+ *
+ * =============================================== */
+
+
+#ifndef CYAPICALLBACKS_H
+#define CYAPICALLBACKS_H
+    
+
+    /*Define your macro callbacks here */
+    /*For more information, refer to the Writing Code topic in the PSoC Creator Help.*/
+
+    
+#endif /* CYAPICALLBACKS_H */   
+/* [] */
