96e50b252722257954e7a6c816def6c14b583be3
Chloé <chloejulie.charlotte@mail.polimi.it>
2022-07-06 17:55:24 +0200

    Comments added to main.py

--- GUI/main.py
+++ GUI/main.py
@@ -146,21 +146,26 @@ class SerialWorker(QRunnable):
         global line, data, old_position_idx, flag_transition, beginning_transition, saved, position_idx
 
         current_date = datetime.datetime.now()
+        # Compute the difference between the data of clicking on start and current data to get current position
         minutes_diff = int(np.floor((((current_date - clicked_date).total_seconds() - 20.0 * (old_position_idx-1))/ 60.0)))
         position_idx = minutes_diff + 1
 
+        # If new position is needed and protocol not ended
         if (position_idx != old_position_idx and position_idx < 13):
-            if flag_transition == 0:
+            # If it is the beginning of the transition
+            if flag_transition == 0:    
                 beginning_transition = current_date
                 flag_transition = 1
 
+            # If 20s have passed since the beginning of the transition, reset flag and match old and new idx
             if round((current_date - beginning_transition).total_seconds() > 20.0):
                 old_position_idx = position_idx
                 flag_transition = 0
 
 
+        # If protocol ended and data not saved (moment just after the end of protocol)
         if (position_idx >= 13 and saved == 0):
-            # Saving data as CSV file
+            # Save data as CSV file
             labels = ['x_chest', 'y_chest', 'z_chest', 'x_ankle', 'y_ankle', 'z_ankle', 'position']
 
             date = datetime.datetime.now()
@@ -169,14 +174,15 @@ class SerialWorker(QRunnable):
 
             with open(csv_name, 'w') as f:
 
-                # using csv.writer method from CSV package
+                # Using csv.writer method from CSV package
                 write = csv.writer(f, delimiter=',', lineterminator='\n')
                 write.writerow(labels)
                 write.writerows(data)
             
             time.sleep(0.01)
-            saved = 1
+            saved = 1   # Update flag to save only once
 
+        # Otherwise, store data coming from readData function
         elif (flag_transition == 0 and saved == 0):
             acc_data = line.copy()
             acc_data.append('{}'.format(minutes_diff+1))
@@ -188,6 +194,7 @@ class SerialWorker(QRunnable):
         """!
         @brief Basic function to send a single char on serial port.
         """
+
         try:
             self.port.write(char.encode('utf-8'))
             logging.info("Written {} on port {}.".format(char, self.port_name))
@@ -221,6 +228,7 @@ class MainWindow(QMainWindow):
         """!
         @brief Init MainWindow.
         """
+
         # Define worker
         self.serial_worker = SerialWorker(None)
 
@@ -247,6 +255,7 @@ class MainWindow(QMainWindow):
         """!
         @brief Set up the graphical interface structure.
         """
+
         # Create the plot widget
         self.graphWidget = PlotWidget()
         
@@ -256,8 +265,8 @@ class MainWindow(QMainWindow):
         self.graphWidget.setTitle("Accelerometer data",color='black', size="10pt")  # Add title
         # Add axis labels
         styles = {'color':'k', 'font-size':'15px'}  
-        self.graphWidget.setLabel('left', 'Acc data [m/s2]', **styles)
-        self.graphWidget.setLabel('bottom', 'Time [pt]', **styles)
+        self.graphWidget.setLabel('left', 'Acceleration', **styles)
+        self.graphWidget.setLabel('bottom', 'Time', **styles)
         self.graphWidget.addLegend((1,1))   # Add legend
         self.graphWidget.setMouseEnabled(x=False, y=False)
 
@@ -295,11 +304,12 @@ class MainWindow(QMainWindow):
             checkable= True, 
             toggled = self.dataUpdate
         )
-        self.updateBtn.setDisabled(True)    # disabled by default
+        self.updateBtn.setDisabled(True)    # Disabled by default
 
         verticalLayout.addWidget(self.updateBtn)
         verticalLayout.addWidget(self.graphWidget)
 
+        # Set timers for simultaneous reading and plotting
         self.timer = QtCore.QTimer()
         self.timer.setInterval(50)
 
@@ -323,12 +333,12 @@ class MainWindow(QMainWindow):
         """
         global line
 
-        # Remove the first y element
+        # Remove the first element
         if (self.count < 101):
             self.count += 1
         else:
             self.horAxis = self.horAxis[1:]
-            self.horAxis.append(self.horAxis[-1] + 1)  # Add a new value 1 higher than the last.
+            self.horAxis.append(self.horAxis[-1] + 1)  # Add new time point
 
         x_chest = int(line[0])
         y_chest = int(line[1])
@@ -338,39 +348,39 @@ class MainWindow(QMainWindow):
         z_ankle = int(line[5])
 
         # X-axis chest acceleration
-        self.xChestGraph = self.xChestGraph[1:]
-        self.xChestGraph.append(x_chest)
+        self.xChestGraph = self.xChestGraph[1:]                  # Remove oldest element
+        self.xChestGraph.append(x_chest)                         # Add new element
         self.dataXChest.setData(self.horAxis, self.xChestGraph)  # Update the data
 
         # Y-axis chest acceleration
-        self.yChestGraph = self.yChestGraph[1:]
-        self.yChestGraph.append(y_chest)
+        self.yChestGraph = self.yChestGraph[1:]                  # Remove oldest element
+        self.yChestGraph.append(y_chest)                         # Add new element
         self.dataYChest.setData(self.horAxis, self.yChestGraph)  # Update the data
 
         # Z-axis chest acceleration
-        self.zChestGraph = self.zChestGraph[1:]
-        self.zChestGraph.append(z_chest)
+        self.zChestGraph = self.zChestGraph[1:]                  # Remove oldest element
+        self.zChestGraph.append(z_chest)                         # Add new element
         self.dataZChest.setData(self.horAxis, self.zChestGraph)  # Update the data
 
         # X-axis ankle acceleration
-        self.xAnkleGraph = self.xAnkleGraph[1:]
-        self.xAnkleGraph.append(x_ankle)
+        self.xAnkleGraph = self.xAnkleGraph[1:]                  # Remove oldest element
+        self.xAnkleGraph.append(x_ankle)                         # Add new element
         self.dataXAnkle.setData(self.horAxis, self.xAnkleGraph)  # Update the data
 
         # Y-axis ankle acceleration
-        self.yAnkleGraph = self.yAnkleGraph[1:]
-        self.yAnkleGraph.append(y_ankle)
+        self.yAnkleGraph = self.yAnkleGraph[1:]                  # Remove oldest element
+        self.yAnkleGraph.append(y_ankle)                         # Add new element
         self.dataYAnkle.setData(self.horAxis, self.yAnkleGraph)  # Update the data
 
         # Z-axis ankle acceleration
-        self.zAnkleGraph = self.zAnkleGraph[1:]
-        self.zAnkleGraph.append(z_ankle)
+        self.zAnkleGraph = self.zAnkleGraph[1:]                  # Remove oldest element
+        self.zAnkleGraph.append(z_ankle)                         # Add new element
         self.dataZAnkle.setData(self.horAxis, self.zAnkleGraph)  # Update the data
 
 
     def draw(self):
         """!
-             @brief Draw the plots.
+        @brief Draw the plots
         """
         global flatLine
 
@@ -384,26 +394,28 @@ class MainWindow(QMainWindow):
     
     def plot(self, graph, x, y, curve_name, color):
         """!
-        @brief Draw graph.
+        @brief Draw graph
         """
+
         pen = pg.mkPen(color=color,width=3)
         line = graph.plot(x, y, name=curve_name, pen=pen)
         return line
 
+
     def updateTextBox(self):
         """!
         @brief Update the textbox output to notify the user about the ongoing protocol
+               (sampling, change position, end protocol)
         """
-
         global position_names, position_idx, flag_transition, saved
 
         if saved == 0:
-            if flag_transition == 0:
+            if flag_transition == 0: 
                 self.textbox.setText('Sampling position {}'.format(position_names[position_idx-1]))
             elif flag_transition == 1 and position_idx < 13:
                 self.textbox.setText('Go to position {} within the next 20 seconds'.format(position_names[position_idx-1]))
         
-        else:
+        else:   # Notify about the end of the protocol - acceleration data still plotted
             self.textbox.setText('End of the protocol - Data saved')
 
 
@@ -412,21 +424,22 @@ class MainWindow(QMainWindow):
     ####################
     def serialscan(self):
         """!
-        @brief Scans all serial ports and creates a list.
+        @brief Scans all serial ports and creates a list
         """
-        # create the combo box to host port list
+
+        # Create the combo box to host port list
         self.port_text = ""
         self.com_list_widget = QComboBox()
         self.com_list_widget.currentTextChanged.connect(self.port_changed)
 
-        # create the connection button
+        # Create the connection button
         self.conn_btn = QPushButton(
             text=("Connect to port {}".format(self.port_text)),
             checkable=True,
             toggled=self.on_toggle
         )
 
-        # acquire list of serial ports and add it to the combo box
+        # Acquire list of serial ports and add it to the combo box
         serial_ports = [
                 p.name
                 for p in serial.tools.list_ports.comports()
@@ -440,8 +453,9 @@ class MainWindow(QMainWindow):
 
     def port_changed(self):
         """!
-        @brief Update conn_btn label based on selected port.
+        @brief Update conn_btn label based on selected port
         """
+
         self.port_text = self.com_list_widget.currentText()
         self.conn_btn.setText("Connect to port {}".format(self.port_text))
 
@@ -449,67 +463,77 @@ class MainWindow(QMainWindow):
     @pyqtSlot(bool)
     def on_toggle(self, checked):
         """!
-        @brief Allow connection and disconnection from selected serial port.
+        @brief Allow connection and disconnection from selected serial port
         """
+
         if checked:
-            # setup reading worker
-            self.serial_worker = SerialWorker(self.port_text) # needs to be re defined
-            # connect worker signals to functions
+            # Setup reading worker
+            self.serial_worker = SerialWorker(self.port_text) # Needs to be re-defined
+            # Connect worker signals to functions
             self.serial_worker.signals.status.connect(self.check_serialport_status)
             self.serial_worker.signals.device_port.connect(self.connected_device)
-            # execute the worker
+            # Execute the worker
             self.threadpool.start(self.serial_worker)
         else:
-            # kill thread
+            # Kill thread
             self.serial_worker.is_killed = True
             self.serial_worker.killed()
-            self.com_list_widget.setDisabled(False) # enable the possibility to change port
+            self.com_list_widget.setDisabled(False) # Enable the possibility to change port
             self.conn_btn.setText(
                 "Connect to port {}".format(self.port_text)
             )
 
+
     def check_serialport_status(self, port_name, status):
-        """!
-        @brief Handle the status of the serial port connection.
-        Available status:
-            - 0  --> Error during opening of serial port
-            - 1  --> Serial port opened correctly
         """
+        @brief Handle the status of the serial port connection
+               Available status:
+                   - 0  --> Error during opening of serial port
+                   - 1  --> Serial port opened correctly
+        """
+
         if status == 0:
             self.conn_btn.setChecked(False)
             
         elif status == 1:
-            # enable all the widgets on the interface
-            self.com_list_widget.setDisabled(True) # disable the possibility to change COM port when already connected
+            # Enable all the widgets on the interface
+            self.com_list_widget.setDisabled(True) # Disable the possibility to change COM port when already connected
             self.conn_btn.setText(
                 "Disconnect from port {}".format(port_name)
             )
             self.updateBtn.setDisabled(False)
 
+
     def connected_device(self, port_name):
         """!
-        @brief Checks on the termination of the serial worker.
+        @brief Checks on the termination of the serial worker
         """
+
         logging.info("Port {} closed.".format(port_name))
         self.updateBtn.setDisabled(True)
 
 
     def ExitHandler(self):
         """!
-        @brief Kill every possible running thread upon exiting application.
+        @brief Kill every possible running thread upon exiting application
         """
+
         self.serial_worker.is_killed = True
         self.serial_worker.killed()
 
 
     def dataUpdate(self,checked):
+        """!
+        @brief Alternatively perform reading and plotting of acc data
+        """
         global line, old_position_idx, flag_transition, saved
 
         clicked_date = datetime.datetime.now()
 
-        if checked:
+        if checked:     # Start
             old_position_idx = 1
-            self.updateBtn.setText("Stop")
+            self.updateBtn.setText("Stop")  # Update button
+            # Start timers
             self.timer.timeout.connect(lambda: self.serial_worker.readData())
             self.timer.start()
             time.sleep(0.1)    # Needed to stabilize the first reading (line)
@@ -522,7 +546,7 @@ class MainWindow(QMainWindow):
             self.textboxTimer.timeout.connect(lambda: self.updateTextBox())
             self.textboxTimer.start() 
 
-        else:
+        else:           # Stop
             # Clear the graph
             self.graphWidget.clear()
             self.horAxis = list(range(100)) 
